770d33c02cb0cff3c7955284e74c5fd437f593f3
cyberyagui <cyberyagui@gmail.com>
2021-09-28 12:22:51 -0300

    Correccion de server y Base de datos

--- api/db/index.js
+++ api/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 /* const config = require('../config') */
 
-const connectionString = `mongodb+srv://cruce:admin@cluster0.ruwjl.mongodb.net/test`
+const connectionString = `mongodb+srv://cruce:admin@cluster0.ruwjl.mongodb.net/CruceDB`
 
 module.exports = async () => {
     await mongoose.connect(connectionString, {
