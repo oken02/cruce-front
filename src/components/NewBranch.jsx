b2a8704d98cc6592c414e7e93fb799ee0e5c1c23
Juan Pablo Lopez Molinari <jplopezmolinari@gmail.com>
2021-09-29 15:38:00 -0300

    ajustes front

--- src/components/NewBranch.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewBranch = () => {
-  return <h1>Acá va el form de order</h1>;
-};
-
-export default NewBranch
--- src/components/NewCourier.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewCourier = () => {
-  return <h1>Acá va el form de mensajería</h1>;
-};
-
-export default NewCourier
