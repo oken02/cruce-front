0e1af715e63c1f97af42d64331ed73452cb8881c
ezevaleriano <ezequiel.valeriano@gmail.com>
2021-09-29 12:19:19 -0300

    probando rutas complejas

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,3 +1,33 @@
-const { User} = require("../models")
+const { User , Courier } = require("../models")
+const bcrypt = require('bcrypt')
 
-module.exports = {}
+//crea un cadete  FALTA MIDDLEWARE 
+const createDeliveryUser = async (req, res,next) => {
+    try {
+    const courierId = "6153322f4f5f727ccd540253"
+    const { fullName, email, dniCuil, password, direction } = req.body
+    const saltRounds = 10
+    const passwordHashed = await bcrypt.hash(password, saltRounds)
+    const courier = await Courier.findOne({_id : courierId})
+    console.log(courierId)
+    console.log(courier)
+
+    const newDeliveryUser = new User({
+        fullName : fullName,
+        email : email,
+        dniCuil : dniCuil,
+        password : passwordHashed,
+        rol : "delivery",
+        direction : direction,
+        courierId : courier
+    })
+    await newDeliveryUser.save()
+    res.status(201).send(newDeliveryUser)
+
+    } 
+    catch (err) {next(err)}
+}
+
+
+
+module.exports = {createDeliveryUser}
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,3 +1,8 @@
+const { createDeliveryUser } = require('../controllers/userControllers');
+const { validateJWT } = require('../middlewares');
 const router = require('express').Router()
+
+
+router.post('/deliveryadd', [validateJWT], createDeliveryUser);
 
 module.exports = router
