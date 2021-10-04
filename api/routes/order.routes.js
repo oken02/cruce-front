ab95507771c1ff61272352e27edf0d6dd244708b
ezevaleriano <ezequiel.valeriano@gmail.com>
2021-10-04 19:10:06 -0300

    Datos dentro de excel posteados

--- api/routes/order.routes.js
+++ api/routes/order.routes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router()
+const { newOrder } = require('../controllers/ecommerceControllers')
+
+
+router.post("/post", newOrder)
+
+router.put("/:pedidoID", [validateJWT, roleMessenger], async (req, res) => {
+    const { id: messengerID } = req.payload;
+  
+    const pedido = await Order.findById(req.params.pedidoID);
+  
+    if (pedido.actualState === "Sin asignar") {
+      pedido.actualState = "Pendiente de retiro en sucursal";
+      pedido.userId = messengerID;
+    } else if (pedido.actualState === "Pendiente de retiro en sucursal") {
+      pedido.actualState = "En camino";
+    } else {
+      pedido.actualState = req.body.newState;
+    }
+  
+    await pedido.save();
+    
+    res.json(pedido);
+  });
 
 module.exports = router
