003e8c52774c9b8918b178e52aab06292a856f6e
Juan Pablo Lopez Molinari <jplopezmolinari@gmail.com>
2021-10-05 13:47:35 -0300

    ajuste variables back

--- src/components/Forms/MessengerEditUser.jsx
+++ src/components/Forms/MessengerEditUser.jsx
@@ -53,7 +53,7 @@ function MessengerEditUser() {
       confirmUserPassword: '',
       fullName: messenger.fullName,
       dniCuil: messenger.dniCuil,
-      direction: messenger.direction,
+      address: messenger.address,
       courierId: messenger.courierId,
     },
     validationSchema: Yup.object({
@@ -229,7 +229,7 @@ function MessengerEditUser() {
 
                     <FormControl as={GridItem} colSpan={6}>
                       <FormLabel
-                        htmlFor="direction"
+                        htmlFor="address"
                         fontSize="sm"
                         fontWeight="md"
                         color={useColorModeValue('gray.700', 'gray.50')}
@@ -238,9 +238,9 @@ function MessengerEditUser() {
                       </FormLabel>
                       <Input
                         type="text"
-                        name="direction"
-                        id="direction"
-                        autoComplete="direction"
+                        name="address"
+                        id="address"
+                        autoComplete="address"
                         mt={1}
                         focusBorderColor="brand.400"
                         shadow="sm"
@@ -249,11 +249,11 @@ function MessengerEditUser() {
                         rounded="md"
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        value={formik.values.direction}
+                        value={formik.values.address}
                       />
-                      {formik.touched.direction && formik.errors.direction ? (
+                      {formik.touched.address && formik.errors.address ? (
                         <div>
-                          <Text color="tomato">{formik.errors.direction}</Text>
+                          <Text color="tomato">{formik.errors.address}</Text>
                         </div>
                       ) : null}
                     </FormControl>
