7e178c372b3568bdeac1c753f4525441786266eb
Ezequiel Valeriano <68381453+EzeValeriano@users.noreply.github.com>
2021-10-12 17:22:07 -0300

    rutas y logica metricas

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,19 +3,21 @@ const router = express.Router();
 const validateJWT = require('../middlewares/validateJWT');
 const { roleEcommerce } = require('../middlewares/validateRole');
 
-
+const UserCourier = require("./userCourier.routes")
 const CourierRouter = require("./courier.routes");
 const BranchRouter = require("./branch.routes");
 const OrderRouter = require("./order.routes");
 const UserRouter = require("./user.routes");
 const authRouter = require("./auth.routes");
 
- 
+
+router.use("/usercourier", [validateJWT, roleEcommerce], UserCourier)
 router.use("/courier", [validateJWT, roleEcommerce ], CourierRouter);
 router.use("/branch", [validateJWT, roleEcommerce ], BranchRouter);
-router.use("/order",[validateJWT], OrderRouter);
+router.use("/order", OrderRouter);
 router.use("/user", [validateJWT] , UserRouter);
 router.use("/auth", authRouter); 
+
 
 
 
