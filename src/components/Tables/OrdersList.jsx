cd870b283cd7011fcf3594b93b11bae20080cc48
idorado <doradoignacio@gmail.com>
2021-10-13 11:41:25 -0300

    filtro estados

--- src/components/Tables/OrdersList.jsx
+++ src/components/Tables/OrdersList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import {
   TableContainer,
@@ -7,28 +7,28 @@ import {
   TableRow,
   TableBody,
   Card,
-} from '@material-ui/core';
-import { Box } from '@chakra-ui/layout';
-import { Button, IconButton } from '@chakra-ui/button';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import { SearchIcon } from '@chakra-ui/icons';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+} from "@material-ui/core";
+import { Box, Center, Spacer } from "@chakra-ui/layout";
+import { Button, IconButton } from "@chakra-ui/button";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Select, Flex, HStack } from "@chakra-ui/react";
 
 //Components
-import TablesHead from './TablesHead';
-import SearchBar from './SearchBar';
-import AlertDeleteOrder from './Alerts/AlertDeleteOrder';
+import TablesHead from "./TablesHead";
+import SearchBar from "./SearchBar";
+import AlertDeleteOrder from "./Alerts/AlertDeleteOrder";
 
 //Hooks
-import useTables from '../../hooks/useTables';
-import getToken from '../../utils/getToken';
-
+import useTables from "../../hooks/useTables";
+import getToken from "../../utils/getToken";
 
 const OrderList = () => {
   const history = useHistory();
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const user = useSelector((state) => state.user);
 
   const {
@@ -45,20 +45,37 @@ const OrderList = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/api/order', getToken())
+      .get("http://localhost:3001/api/order", getToken())
       .then((res) => setOrders(res.data));
   }, []);
+
+  const onState = (state) => {
+    axios
+      .post(
+        `http://localhost:3001/api/order/ordersbystate`,
+        { state: `${state}` },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((data) => {
+        console.log("Estado", data.data);
+        setOrders(data.data);
+      });
+  };
 
   return (
     <Box p="4">
       <Box display="flex" justifyContent="space-between" mb="4">
         <h1>Todos los Pedidos</h1>
 
-        {user.loggedUser.role === 'ecommerce' ? (
+        {user.loggedUser.role === "ecommerce" ? (
           <Button
             colorScheme="teal"
             size="sm"
-            onClick={() => history.push('/dashboard/order')}
+            onClick={() => history.push("/dashboard/order")}
           >
             Subir Pedidos
           </Button>
@@ -67,12 +84,40 @@ const OrderList = () => {
 
       <Card>
         <TableContainer>
-          <Box p="3">
-            <SearchBar
-              searchText={searchText}
-              onSearchText={handleSearchText}
-            />
-          </Box>
+          <Flex>
+            <HStack spacing="24px">
+              <Box p="6">
+                <Select
+                  placeholder="Estado"
+                  size="sm"
+                  onChange={(e) => {
+                    onState(e.target.value);
+                    console.log("E Target", e.target.value);
+                  }}
+                >
+                  {/* <option value="Todos">Todos</option> */}
+                  <option value="Sin Asignar">Sin Asignar</option>
+                  <option value="Pendiente de Retiro en Sucursal">
+                    Pendiente de retiro
+                  </option>
+                  <option value="En Camino">En Camino</option>
+                  <option value="Entregado">Entregado</option>
+                  <option value="Devuelto a Sucursal">
+                    Devuelto a Sucursal
+                  </option>
+                </Select>
+              </Box>
+
+              <Spacer />
+              <Box>
+                <SearchBar
+                  searchText={searchText}
+                  onSearchText={handleSearchText}
+                />
+              </Box>
+            </HStack>
+          </Flex>
+
           <Table size="small">
             <TablesHead
               order={order}
@@ -85,7 +130,7 @@ const OrderList = () => {
                   <TableRow hover key={index.toString()} tabIndex={-1}>
                     <TableCell>
                       <Link to={`/dashboard/order/${row._id}`}>
-                        {row.orderId}{' '}
+                        {row.orderId}{" "}
                       </Link>
                     </TableCell>
                     <TableCell>
@@ -94,17 +139,14 @@ const OrderList = () => {
                     <TableCell>{row.dniCuil}</TableCell>
                     <TableCell>{row.actualState}</TableCell>
                     <TableCell>
-                      {row.courierId ? row.courierId.name : ''}
+                      {row.courierId ? row.courierId.name : ""}
                     </TableCell>
 
                     <TableCell>
-                      <AlertDeleteOrder
-                      ordID={row._id}
-                      name={row.orderID}
-                      />
+                      <AlertDeleteOrder ordID={row._id} name={row.orderID} />
 
                       <Link to={`/dashboard/order/${row._id}`}>
-                      <IconButton
+                        <IconButton
                           variant="ghost"
                           colorScheme="teal"
                           fontSize="20 px"
