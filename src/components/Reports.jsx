5107018af9170ad8c9b84a6f069fb2547df324b7
Juan Pablo Lopez Molinari <jplopezmolinari@gmail.com>
2021-10-13 19:09:05 -0300

    ajuste metricas mobile

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -6,15 +6,29 @@ import {
   StatHelpText,
   StatArrow,
   StatGroup,
-   chakra, Box, Flex, useColorModeValue, Link
-} from "@chakra-ui/react"
-import { TriangleUpIcon } from '@chakra-ui/icons'
+  chakra,
+  Box,
+  Flex,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
+import { TriangleUpIcon } from '@chakra-ui/icons';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import getToken from '../utils/getToken';
-import {  calculateDelivered, deliveredAvg, calculateNotAsign, notAsignAvg, calculateOnItsWay, onItsWayAvg, calculatePendings, pendingsAvg, calculateReturned,returnedAvg }from "../utils/metrics"
-
+import {
+  calculateDelivered,
+  deliveredAvg,
+  calculateNotAsign,
+  notAsignAvg,
+  calculateOnItsWay,
+  onItsWayAvg,
+  calculatePendings,
+  pendingsAvg,
+  calculateReturned,
+  returnedAvg,
+} from '../utils/metrics';
 
 const Reports = () => {
   const history = useHistory();
@@ -22,258 +36,276 @@ const Reports = () => {
   const user = useSelector((state) => state.user);
   const [orders, setOrders] = useState([]);
 
-  console.log("ORDERS --> ", orders)
+  console.log('ORDERS --> ', orders);
   const { loggedUser } = useSelector((state) => state.user);
   const userId = loggedUser._id;
 
-
   useEffect(() => {
-    if (loggedUser.role === "ecommerce") { 
-    axios
-      .get('http://localhost:3001/api/order', getToken())
-      .then((res) => setOrders(res.data))
-  } else if(loggedUser.role === "messenger") {
-    axios
-    .post(
-      `http://localhost:3001/api/order/myorders/${userId}`,
-      {}, getToken())
-      .then((res) => setOrders(res.data));
-    } else if (loggedUser.role === "courier"){
-      axios.post(
-        "http://localhost:3001/api/order/",
-        {}, getToken())
+    if (loggedUser.role === 'ecommerce') {
+      axios
+        .get('http://localhost:3001/api/order', getToken())
         .then((res) => setOrders(res.data));
-  }
+    } else if (loggedUser.role === 'messenger') {
+      axios
+        .post(
+          `http://localhost:3001/api/order/myorders/${userId}`,
+          {},
+          getToken()
+        )
+        .then((res) => setOrders(res.data));
+    } else if (loggedUser.role === 'courier') {
+      axios
+        .post('http://localhost:3001/api/order/', {}, getToken())
+        .then((res) => setOrders(res.data));
+    }
   }, []);
- 
-  
+
   return (
+    <>
+      <h1>Acá van los reportes</h1>
 
-    <><h1>Acá van los reportes</h1>
+      <h1>Total de pedidos: {orders.length}</h1>
 
-        <h1>Total de pedidos: {orders.length}</h1>
-
-    <Flex
-      bg={useColorModeValue("#F9FAFB", "gray.600")}
-      p={50}
-      w="full"
-      alignItems="center"
-      justifyContent="center"
+      <Flex
+        bg={useColorModeValue('#F9FAFB', 'gray.600')}
+        p={50}
+        direction={['column', 'column', 'row', 'row']}
+        w={['90vw', '90vw', '90vw', 'full']}
+        alignItems="center"
+        justifyContent="center"
       >
-      <Box
-          maxW="sm"
+        <Box
+          width="90vw"
+          height="auto"
           borderWidth="1px"
           px={4}
           py={4}
-          bg={useColorModeValue("white", "gray.800")}
+          bg={useColorModeValue('white', 'gray.800')}
           shadow="md"
           rounded="md"
           m={1}
-      >
-        <Flex justifyContent="space-between" alignItems="center">
+        >
+          <Flex justifyContent="space-between" alignItems="center">
             <chakra.span
-            
-            fontSize="md"
+              fontSize="md"
               fontWeight="semibold"
-              color={useColorModeValue("gray.800", "gray.400")}
-          >
+              color={useColorModeValue('gray.800', 'gray.400')}
+            >
               Sin asignar
             </chakra.span>
-   
-        </Flex>
+          </Flex>
 
-        <Box>
-          <chakra.h1
-            fontSize="lg"
-            fontWeight="bold"
-            mt={2}
-            color={useColorModeValue("gray.800", "white")}
-          >
-    <Stat>
-        {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
+          <Box>
+            <chakra.h1
+              fontSize="lg"
+              fontWeight="bold"
+              mt={2}
+              color={useColorModeValue('gray.800', 'white')}
+              isInline
+              align="baseline"
+            >
+              <Stat>
+                {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
                 <StatNumber>{calculateNotAsign(orders)}</StatNumber>
-        <StatHelpText            fontSize="sm"
-            fontWeight="regular" color="gray.500">
-                  <TriangleUpIcon boxSize={4} />          {notAsignAvg(orders).toFixed(2)}%
-        </StatHelpText>
-     </Stat>
+                <StatHelpText
+                  fontSize="sm"
+                  fontWeight="regular"
+                  color="gray.500"
+                >
+                  <TriangleUpIcon boxSize={4} />{' '}
+                  {notAsignAvg(orders).toFixed(2)}%
+                </StatHelpText>
+              </Stat>
             </chakra.h1>
+          </Box>
         </Box>
-      </Box>
 
-      <Box
-     maxW="sm"
-     borderWidth="1px"
-     px={4}
-     py={4}
-     bg={useColorModeValue("white", "gray.800")}
-     shadow="md"
-     rounded="md"
-     m={1}
-      >
-        <Flex justifyContent="space-between" alignItems="center">
+        <Box
+          width="90vw"
+          height="auto"
+          borderWidth="1px"
+          px={4}
+          py={4}
+          bg={useColorModeValue('white', 'gray.800')}
+          shadow="md"
+          rounded="md"
+          m={1}
+        >
+          <Flex justifyContent="space-between" alignItems="center">
             <chakra.span
-            
-            fontSize="md"
+              fontSize="md"
               fontWeight="semibold"
-              color={useColorModeValue("gray.800", "gray.400")}
-          >
+              color={useColorModeValue('gray.800', 'gray.400')}
+            >
               Para retirar
             </chakra.span>
-        </Flex>
+          </Flex>
 
-        <Box>
-          <chakra.h1
-            fontSize="lg"
-            fontWeight="bold"
-            mt={2}
-            color={useColorModeValue("gray.800", "white")}
-          >
-            <Stat>
+          <Box>
+            <chakra.h1
+              fontSize="lg"
+              fontWeight="bold"
+              mt={2}
+              color={useColorModeValue('gray.800', 'white')}
+              isInline
+              align="baseline"
+            >
+              <Stat>
                 {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
                 <StatNumber>{calculatePendings(orders)}</StatNumber>
-                <StatHelpText            fontSize="sm"
-            fontWeight="regular" color="gray.500">
+                <StatHelpText
+                  fontSize="sm"
+                  fontWeight="regular"
+                  color="gray.500"
+                >
                   <TriangleUpIcon boxSize={4} />
                   {pendingsAvg(orders).toFixed(2)}%
                 </StatHelpText>
-            </Stat>
+              </Stat>
             </chakra.h1>
+          </Box>
         </Box>
-      </Box>
-        
-      <Box
-        maxW="sm"
-        borderWidth="1px"
-        px={4}
-        py={4}
-        bg={useColorModeValue("white", "gray.800")}
-        shadow="md"
-        rounded="md"
-        m={1}
-      >
-        <Flex justifyContent="space-between" alignItems="center">
+
+        <Box
+          width="90vw"
+          height="auto"
+          borderWidth="1px"
+          px={4}
+          py={4}
+          bg={useColorModeValue('white', 'gray.800')}
+          shadow="md"
+          rounded="md"
+          m={1}
+        >
+          <Flex justifyContent="space-between" alignItems="center">
             <chakra.span
-            
-            fontSize="md"
+              fontSize="md"
               fontWeight="semibold"
-              color={useColorModeValue("gray.800", "gray.400")}
-          >
+              color={useColorModeValue('gray.800', 'gray.400')}
+            >
               En camino
             </chakra.span>
-        </Flex>
+          </Flex>
 
-        <Box>
-          <chakra.h1
-            fontSize="lg"
-            fontWeight="bold"
-            mt={2}
-            color={useColorModeValue("gray.800", "white")}
-          >
-            <Stat>
+          <Box>
+            <chakra.h1
+              fontSize="lg"
+              fontWeight="bold"
+              mt={2}
+              color={useColorModeValue('gray.800', 'white')}
+              isInline
+              align="baseline"
+            >
+              <Stat>
                 {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
                 <StatNumber>{calculateOnItsWay(orders)}</StatNumber>
-                <StatHelpText            fontSize="sm"
-            fontWeight="regular" color="gray.500">
+                <StatHelpText
+                  fontSize="sm"
+                  fontWeight="regular"
+                  color="gray.500"
+                >
                   <TriangleUpIcon boxSize={4} />
                   {onItsWayAvg(orders).toFixed(2)}%
                 </StatHelpText>
-            </Stat>
+              </Stat>
             </chakra.h1>
+          </Box>
         </Box>
-      </Box>
 
-      <Box
-      maxW="sm"
-      borderWidth="1px"
-      px={4}
-      py={4}
-      bg={useColorModeValue("white", "gray.800")}
-      shadow="md"
-      rounded="md"
-      m={1}
-      >
-        <Flex justifyContent="space-between" alignItems="center">
+        <Box
+          width="90vw"
+          height="auto"
+          borderWidth="1px"
+          px={4}
+          py={4}
+          bg={useColorModeValue('white', 'gray.800')}
+          shadow="md"
+          rounded="md"
+          m={1}
+        >
+          <Flex justifyContent="space-between" alignItems="center">
             <chakra.span
-            
-            fontSize="md"
+              fontSize="md"
               fontWeight="semibold"
-              color={useColorModeValue("gray.800", "gray.400")}
-          >
+              color={useColorModeValue('gray.800', 'gray.400')}
+            >
               Entregados
             </chakra.span>
-        </Flex>
+          </Flex>
 
-        <Box>
-          <chakra.h1
-            fontSize="lg"
-            fontWeight="bold"
-            mt={2}
-            color={useColorModeValue("gray.800", "white")}
-          >
-            <Stat>
+          <Box>
+            <chakra.h1
+              fontSize="lg"
+              fontWeight="bold"
+              mt={2}
+              color={useColorModeValue('gray.800', 'white')}
+              isInline
+              align="baseline"
+            >
+              <Stat>
                 {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
                 <StatNumber>{calculateDelivered(orders)}</StatNumber>
-                <StatHelpText            fontSize="sm"
-            fontWeight="regular" color="gray.500">
+                <StatHelpText
+                  fontSize="sm"
+                  fontWeight="regular"
+                  color="gray.500"
+                >
                   <TriangleUpIcon boxSize={4} />
                   {deliveredAvg(orders).toFixed(2)}%
                 </StatHelpText>
-            </Stat>
+              </Stat>
             </chakra.h1>
+          </Box>
         </Box>
-        </Box>
-        
+
         <Box
-     maxW="sm"
-     borderWidth="1px"
-     px={4}
-     py={4}
-     bg={useColorModeValue("white", "gray.800")}
-     shadow="md"
-     rounded="md"
-     m={1}      >
-        <Flex justifyContent="space-between" alignItems="center">
+          width="90vw"
+          height="auto"
+          borderWidth="1px"
+          px={4}
+          py={4}
+          bg={useColorModeValue('white', 'gray.800')}
+          shadow="md"
+          rounded="md"
+          m={1}
+        >
+          <Flex justifyContent="space-between" alignItems="center">
             <chakra.span
-            
-            fontSize="md"
+              fontSize="md"
               fontWeight="semibold"
-              color={useColorModeValue("gray.800", "gray.400")}
-          >
+              color={useColorModeValue('gray.800', 'gray.400')}
+            >
               Devueltos
             </chakra.span>
-        </Flex>
+          </Flex>
 
-        <Box>
-          <chakra.h1
-            fontSize="lg"
-            fontWeight="bold"
-            mt={2}
-            color={useColorModeValue("gray.800", "white")}
-          >
-            <Stat>
+          <Box>
+            <chakra.h1
+              fontSize="lg"
+              fontWeight="bold"
+              mt={2}
+              color={useColorModeValue('gray.800', 'white')}
+              isInline
+              align="baseline"
+            >
+              <Stat>
                 {/* <StatLabel>Pedidos sin asignar</StatLabel> */}
                 <StatNumber>{calculateReturned(orders)}</StatNumber>
-                <StatHelpText            fontSize="sm"
-            fontWeight="regular" color="gray.500">
+                <StatHelpText
+                  fontSize="sm"
+                  fontWeight="regular"
+                  color="gray.500"
+                >
                   <TriangleUpIcon boxSize={4} />
                   {returnedAvg(orders).toFixed(2)}%
                 </StatHelpText>
-            </Stat>
+              </Stat>
             </chakra.h1>
+          </Box>
         </Box>
-      </Box>
-      
-    </Flex>
-
-
-
-
-      
-    
+      </Flex>
     </>
-  )
- 
+  );
 };
 
-export default Reports
+export default Reports;
